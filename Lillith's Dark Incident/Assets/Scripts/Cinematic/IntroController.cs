5a421bf3e9db64d01a8ddff0a89ac8aece61ff0c
SebzSZ <zupremacia@gmail.com>
2023-10-24 23:00:28 -0600

    Alpha 0.0.4 + ejecutable

--- Lillith's Dark Incident/Assets/Scripts/Cinematic/IntroController.cs
+++ Lillith's Dark Incident/Assets/Scripts/Cinematic/IntroController.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
-using UnityEditorInternal;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
